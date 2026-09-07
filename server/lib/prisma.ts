350976ef09ef87b8f187505ffbd769d032d89c87
Victor-430 <oluwatobivictor430@gmail.com>
2026-09-07 04:18:20 +0100

    change tsconfig module

--- server/lib/prisma.ts
+++ server/lib/prisma.ts
@@ -1,3 +1,8 @@
+// import { PrismaClient } from "../prisma/generated/client";
+// import { PrismaClient } from "@prisma/client";
+
+// export const prisma = new PrismaClient();
+
 import { PrismaClient } from "@prisma/client";
 
 export const prisma = new PrismaClient();
